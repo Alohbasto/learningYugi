9076bf53d4ff0651c095ea449fa902861d3eb685
Alohbasto <aconbal812@g.educaand.es>
2026-02-14 23:31:56 +0100

    mod 5

--- aprender-yugi/astro.config.mjs
+++ aprender-yugi/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Alohbasto.github.io', 
+	site: 'https://alohbasto.github.io', 
   base: '/aprender-yugi'
 });
